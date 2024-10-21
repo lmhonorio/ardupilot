98fab8a9483a75c82464ba1a6ec8bad46e6eb939
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2024-10-21 15:43:03 -0300

    Code refactoring, removing several hardcoded values. Possible bug fix for trim values

--- Rover/scripts/aeroboat-controlallocation.lua
+++ Rover/scripts/aeroboat-controlallocation.lua
@@ -16,13 +16,14 @@ CONTROL_OUTPUT_THROTTLE = 3
 CONTROL_OUTPUT_YAW = 4
 TRIM3 = 1500
 TRIM1 = 1500
+LOW_TRIM = 1100
+MID_TRIM = 1500
+RADIO_CHANNEL_BANDWIDTH = 450
 SYSTEM_STARTED = Parameter()
 local desired_yaw = -1.0
 
-
-local steering_pid = PID:new(0.25, 0.001, 0.1, 0.8, -0.8, 0.8, -0.8)  -- Configure os ganhos como necessários
+-- local steering_pid = PID:new(0.25, 0.001, 0.1, 0.8, -0.8, 0.8, -0.8)  -- Configure os ganhos como necessários
 local steering_pid = PID:new(0.25, 0.001, 0.01, 0.8, -0.8, 0.8, -0.8) -- Configure os ganhos como necessários
-
 
 -- Severity for logging in GCS
 MAV_SEVERITY = { EMERGENCY = 0, ALERT = 1, CRITICAL = 2, ERROR = 3, WARNING = 4, NOTICE = 5, INFO = 6, DEBUG = 7 }
@@ -35,18 +36,21 @@ It basically calculates the norm of the desired control vector (t,s)
 and then splits it into the right and left side
 --]]
 local function new_control_allocation(t, s)
-  -- Calculating the norm of each component on the desired control vector (t,s)
+  -- eps not to divide by zero
+  local eps = 0.0001
+
+  -- Calculating the norm of each component of the desired control vector (t,s)
   local aloc = 400
 
-  local hip = math.sqrt(t * t + s * s) + 0.0001
+  local hip = math.sqrt(t * t + s * s) + eps
 
   local nTa = aloc * t / hip
   local nSa = aloc * s / hip
 
-  local T = math.abs(nTa / (math.abs(nTa) + math.abs(nSa) + 0.0001))
-  local S = math.abs(nSa / (math.abs(nTa) + math.abs(nSa) + 0.0001))
+  local T = math.abs(nTa / (math.abs(nTa) + math.abs(nSa) + eps))
+  local S = math.abs(nSa / (math.abs(nTa) + math.abs(nSa) + eps))
 
-  -- Splitting on the right and left side
+  -- Splitting between right and left sides
   local nft = t * T * aloc
   local nfs = s * S * aloc
 
@@ -119,12 +123,12 @@ local function update()
     TRIM1 = param:get('RC1_TRIM')
 
     -- Send the PWM trim (neutral) values to the servo outputs
-    local pwm0_trim_value = tonumber(param:get('SERVO1_TRIM')) or 1500
-    local pwm1_trim_value = tonumber(param:get('SERVO2_TRIM')) or 1500
-    local pwm2_trim_value = tonumber(param:get('SERVO3_TRIM')) or 1500
-    local pwm3_trim_value = tonumber(param:get('SERVO4_TRIM')) or 1500
-    local pwm4_trim_value = tonumber(param:get('SERVO5_TRIM')) or 1500
-    local pwm5_trim_value = tonumber(param:get('SERVO6_TRIM')) or 1500
+    local pwm0_trim_value = tonumber(param:get('SERVO1_TRIM')) or LOW_TRIM
+    local pwm1_trim_value = tonumber(param:get('SERVO2_TRIM')) or LOW_TRIM
+    local pwm2_trim_value = tonumber(param:get('SERVO3_TRIM')) or LOW_TRIM
+    local pwm3_trim_value = tonumber(param:get('SERVO4_TRIM')) or LOW_TRIM
+    local pwm4_trim_value = tonumber(param:get('SERVO5_TRIM')) or LOW_TRIM
+    local pwm5_trim_value = tonumber(param:get('SERVO6_TRIM')) or LOW_TRIM
     SRV_Channels:set_output_pwm_chan_timeout(0, pwm0_trim_value, 3000)
     SRV_Channels:set_output_pwm_chan_timeout(1, pwm1_trim_value, 3000)
     SRV_Channels:set_output_pwm_chan_timeout(2, pwm2_trim_value, 3000)
@@ -143,14 +147,14 @@ local function update()
 
   if vehicle:get_mode() == DRIVING_MODES.MANUAL then
     -- Get the trim values for the RC channels
-    TRIM3 = param:get('RC3_TRIM')
-    TRIM1 = param:get('RC1_TRIM')
+    TRIM3 = tonumber(param:get('RC3_TRIM'))
+    TRIM1 = tonumber(param:get('RC1_TRIM'))
     -- Get the RC PWM values
-    rc3_pwm = tonumber(rc:get_pwm(3)) or 1500
-    rc1_pwm = tonumber(rc:get_pwm(1)) or 1500
+    rc3_pwm = tonumber(rc:get_pwm(3)) or TRIM3 or MID_TRIM
+    rc1_pwm = tonumber(rc:get_pwm(1)) or TRIM1 or MID_TRIM
     -- Transform the RC PWM values to the desired control values and send to allocation
-    throttle = (TRIM3 - rc3_pwm) / 450
-    steering = (rc1_pwm - TRIM1) / 450
+    throttle = (TRIM3 - rc3_pwm) / RADIO_CHANNEL_BANDWIDTH
+    steering = (rc1_pwm - TRIM1) / RADIO_CHANNEL_BANDWIDTH
     new_control_allocation(throttle, steering)
 
     return update, 200
@@ -160,12 +164,12 @@ local function update()
     end
 
     TRIM3 = param:get('RC3_TRIM')
-    rc3_pwm = tonumber(rc:get_pwm(3)) or 1500
-    throttle = (TRIM3 - rc3_pwm) / 450
+    rc3_pwm = tonumber(rc:get_pwm(3)) or TRIM3 or MID_TRIM
+    throttle = (TRIM3 - rc3_pwm) / RADIO_CHANNEL_BANDWIDTH
 
     TRIM1 = param:get('RC1_TRIM')
-    rc1_pwm = tonumber(rc:get_pwm(1)) or 1500
-    local addsteering = (rc1_pwm - TRIM1) / 450
+    rc1_pwm = tonumber(rc:get_pwm(1)) or TRIM1 or MID_TRIM
+    local addsteering = (rc1_pwm - TRIM1) / RADIO_CHANNEL_BANDWIDTH
 
     local k1 = param:get('SCR_USER4')
 
@@ -176,25 +180,26 @@ local function update()
     steering_pid.P = param:get('SCR_USER3')
     steering_pid.I = param:get('SCR_USER2')
     steering_pid.D = param:get('SCR_USER1')
-    local mysteering = steering_pid:compute(0, steering_error, 0.2)
+    local auto_steering = steering_pid:compute(0, steering_error, 0.2)
 
-    new_control_allocation(throttle, mysteering)
+    new_control_allocation(throttle, auto_steering)
 
     return update, 200
+
   elseif vehicle:get_mode() == DRIVING_MODES.AUTO or vehicle:get_mode() == DRIVING_MODES.GUIDED then
     if desired_yaw == -1 then
       desired_yaw = funcs:map_to_360(funcs:to_degrees(ahrs:get_yaw()))
     end
 
-    local mysteering, vh_yaw, steering_error = 0, 0, 0
+    local auto_steering, vh_yaw, steering_error = 0, 0, 0
 
     TRIM3 = param:get('RC3_TRIM')
-    rc3_pwm = tonumber(rc:get_pwm(3)) or 1500
-    throttle = (TRIM3 - rc3_pwm) / 450
+    rc3_pwm = tonumber(rc:get_pwm(3)) or TRIM3 or MID_TRIM
+    throttle = (TRIM3 - rc3_pwm) / RADIO_CHANNEL_BANDWIDTH
 
     TRIM1 = param:get('RC1_TRIM')
-    rc1_pwm = tonumber(rc:get_pwm(1)) or 1500
-    steering = (rc1_pwm - TRIM1) / 450
+    rc1_pwm = tonumber(rc:get_pwm(1)) or TRIM1 or MID_TRIM
+    steering = (rc1_pwm - TRIM1) / RADIO_CHANNEL_BANDWIDTH
 
     if math.abs(steering) > 0.05 then
       desired_yaw = funcs:map_to_360(funcs:to_degrees(ahrs:get_yaw()))
@@ -202,13 +207,13 @@ local function update()
     else
       vh_yaw = funcs:map_to_360(funcs:to_degrees(ahrs:get_yaw()))
       steering_error = funcs:map_error(desired_yaw - vh_yaw)
-      mysteering = steering_pid:compute(0, steering_error, 0.2)
-      new_control_allocation(throttle, mysteering)
+      auto_steering = steering_pid:compute(0, steering_error, 0.2)
+      new_control_allocation(throttle, auto_steering)
     end
 
     return update, 200
   else
-    -- Get the control outputs from the vehicle and pass into control allocation
+    -- Get the control outputs from the vehicle and pass them directly to control allocation
     steering = tonumber(vehicle:get_control_output(CONTROL_OUTPUT_YAW)) or 0
     throttle = tonumber(vehicle:get_control_output(CONTROL_OUTPUT_THROTTLE)) or 0
     --gcs:send_text(4, string.format("t,s = %f ; %f",  throttle, steering))
@@ -216,6 +221,6 @@ local function update()
 
     return update, 200
   end
-end                 -- update function
+end -- update function
 
 return update, 3000 -- run immediately before starting to reschedule
