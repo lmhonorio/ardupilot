c9a6f1dfef43112b52acac7b44cd723ee31cbb96
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2025-01-02 16:59:06 -0300

    refactoring pid module

--- Rover/scripts/modules/pid.lua
+++ Rover/scripts/modules/pid.lua
@@ -8,15 +8,15 @@ PID.__index = PID
 
 function PID:new(p_gain, i_gain, d_gain, i_max, i_min, pid_max, pid_min)
   local obj = {
-      P = p_gain or 0,
-      I = i_gain or 0,
-      D = d_gain or 0,
-      integrator = 0,
-      last_error = 0,
-      i_max = i_max or 0,
-      i_min = i_min or 0,
-      pid_max = pid_max or 1,
-      pid_min = pid_min or -1
+    P = p_gain or 0,
+    I = i_gain or 0,
+    D = d_gain or 0,
+    integrator = 0,
+    last_error = 0,
+    i_max = i_max or 0,
+    i_min = i_min or 0,
+    pid_max = pid_max or 1,
+    pid_min = pid_min or -1
   }
   setmetatable(obj, self)
   -- self.__index = self
@@ -36,13 +36,19 @@ function PID:setD(d_gain)
   self.D = tonumber(d_gain)
 end
 
-function PID:compute(setpoint, pv, dt)
-  local error = setpoint - pv
-  local deriv = (error - self.last_error)/dt
-  self.integrator = self.integrator + error*dt
-  self.integrator = math.max(math.min(self.integrator, self.i_max), self.i_min)
+function PID:limitRange(value, min, max)
+  return math.min(math.max(value, min), max)
+end
 
-  local output = math.min(math.max(self.P*error + self.I*self.integrator + self.D*deriv, self.pid_min), self.pid_max)
+function PID:compute(setpoint, current_value, dt)
+  local error = setpoint - current_value
+  local deriv = (error - self.last_error) / dt
+  self.integrator = self.integrator + error * dt
+  self.integrator = self:limitRange(self.integrator, self.i_min, self.i_max)
+
+  local pid_value = self.P * error + self.I * self.integrator + self.D * deriv
+  local output = self:limitRange(pid_value, self.pid_min, self.pid_max)
+
   self.last_error = error
 
   return output
