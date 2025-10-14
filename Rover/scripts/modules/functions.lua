2074e10c76964da9ee1547d80f68e425c2e0c010
viniciusvidal2 <viniciusvidal2@gmail.com>
2025-10-14 13:23:32 -0300

    removing unused files and deprecated functions now that we use pixhawk controller output

--- Rover/scripts/modules/functions.lua
+++ Rover/scripts/modules/functions.lua
@@ -7,18 +7,6 @@ local funcs = {}
 funcs.__index = funcs
 
 --[[
-Converts degrees to radians
--- @param mdegrees number - Angle in degrees
--- @return number - Angle in radians
---]]
-function funcs:toRadians(mdegrees)
-  if mdegrees == nil then
-    return 0
-  end
-  return mdegrees * math.pi / 180.0
-end
-
---[[
 Converts radians to degrees
 -- @param mradians number - Angle in radians
 -- @return number - Angle in degrees
@@ -28,18 +16,6 @@ function funcs:toDegrees(mradians)
     return 0
   end
   return mradians * 180.0 / math.pi
-end
-
---[[
-Dot product of two 2D vectors
--- @param u_x number - X component of the first vector
--- @param u_y number - Y component of the first vector
--- @param v_x number - X component of the second vector
--- @param v_y number - Y component of the second vector
--- @return number - The dot product of the two vectors
---]]
-function funcs:dotProduct(u_x, u_y, v_x, v_y)
-  return u_x * v_x + u_y * v_y
 end
 
 --[[
@@ -60,21 +36,6 @@ function funcs:mapMaxMin(value, min, max)
 end
 
 --[[
-Making sure the error is within the range [-180, 180]
--- @param input_error number - The input error value
--- @return number - The mapped error value within the range [-180, 180]
---]]
-function funcs:mapErrorToRange(input_error)
-  if input_error > -180 and input_error < 180 then
-    return input_error
-  elseif input_error < -180 then
-    return input_error + 360
-  else
-    return input_error - 360
-  end
-end
-
---[[
 Function to map an angle to the range [0, 360]
 -- @param angle number - The input angle in degrees
 -- @return number - The mapped angle in the range [0, 360]
@@ -87,74 +48,6 @@ function funcs:mapTo360(angle)
     return angle + 360
   end
   return angle
-end
-
---[[
-Calculates orientation based on two pairs of geographic coordinates (the vector from one to the other)
-First point is the origin, second on is the target of our orientation.
-0 degrees should be north, 90 degrees east
--- @param lat_1 number - Latitude of the first point in degrees
--- @param lon_1 number - Longitude of the first point in degrees
--- @param lat_2 number - Latitude of the second point in degrees
--- @param lon_2 number - Longitude of the second point in degrees
--- @return number - Bearing in degrees from the first point to the second point [0, 360]
---]]
-function funcs:calculateBearingBetweenPoints(lat_1, lon_1, lat_2, lon_2)
-  local rad_lat_1, rad_lon_1 = funcs:toRadians(lat_1), funcs:toRadians(lon_1)
-  local rad_lat_2, rad_lon_2 = funcs:toRadians(lat_2), funcs:toRadians(lon_2)
-  local d_lon = rad_lon_2 - rad_lon_1
-  local y = math.sin(d_lon) * math.cos(rad_lat_2)
-  local x = math.cos(rad_lat_1) * math.sin(rad_lat_2) - math.sin(rad_lat_1) * math.cos(rad_lat_2) * math.cos(d_lon)
-  local bearing = math.atan(y, x)
-  -- Transform to match the ardupilot convention and map to [0, 360]
-  bearing = funcs:toDegrees(bearing)
-  return (bearing + 360) % 360
-end
-
---[[
-Calculate distance (latlon to meters) using the Haversine formula
--- @param lat_1 number - Latitude of the first point in degrees
--- @param lon_1 number - Longitude of the first point in degrees
--- @param lat_2 number - Latitude of the second point in degrees
--- @param lon_2 number - Longitude of the second point in degrees
--- @return number - Distance between the two points in meters
---]]
-function funcs:haversineDistance(lat_1, lon_1, lat_2, lon_2)
-  local R = 6371000 -- Earth radius [m]
-  local rad_lat_1, rad_lon_1 = funcs:toRadians(lat_1), funcs:toRadians(lon_1)
-  local rad_lat_2, rad_lon_2 = funcs:toRadians(lat_2), funcs:toRadians(lon_2)
-  local deltaLat = rad_lat_2 - rad_lat_1
-  local deltaLon = rad_lon_2 - rad_lon_1
-
-  local a = math.sin(deltaLat / 2) ^ 2 +
-      math.cos(rad_lat_1) * math.cos(rad_lat_2) *
-      math.sin(deltaLon / 2) ^ 2
-
-  local c = 2 * math.atan(math.sqrt(a), math.sqrt(1 - a))
-
-  return R * c
-end
-
---[[
-Project a point (p) onto a line defined by start (s) and end (e) points
--- @param p_x number - X coordinate of the point to be projected
--- @param p_y number - Y coordinate of the point to be projected
--- @param s_x number - X coordinate of the start point of the line
--- @param s_y number - Y coordinate of the start point of the line
--- @param e_x number - X coordinate of the end point of the line
--- @param e_y number - Y coordinate of the end point of the line
--- @return number, number - The projected point coordinates (x, y)
---]]
-function funcs:lineProjectionPoint(p_x, p_y, s_x, s_y, e_x, e_y)
-  -- Calculate the projection scalar t of our current (p)
-  -- on top of the line formed from start (s) to end (e) points
-  local sp_x, sp_y = p_x - s_x, p_y - s_y
-  local se_x, se_y = e_x - s_x, e_y - s_y
-  local se_length_squared = se_x ^ 2 + se_y ^ 2
-  local t = funcs:dotProduct(sp_x, sp_y, se_x, se_y) / se_length_squared
-
-  -- Return the projected point coordinates based on the scaler t
-  return s_x + t * se_x, s_y + t * se_y
 end
 
 --[[
@@ -196,56 +89,6 @@ function funcs:allocateRightAndLeftPwmShare(t, s, pwm_range)
   local pwm_aloc_l = (t_share + s_share) * pwm_range
   local pwm_aloc_r = (t_share - s_share) * pwm_range
   return math.floor(pwm_aloc_l), math.floor(pwm_aloc_r)
-end
-
---[[
-Calculates the cross-track error using vehicle and projection coordinates.
--- @param velocity The vehicle's current velocity.
--- @param gain The gain constant for the cross-track correction.
--- @param p_x The x-coordinate of the projection point on the path.
--- @param p_y The y-coordinate of the projection point on the path.
--- @param vh_x The vehicle's x-coordinate.
--- @param vh_y The vehicle's y-coordinate.
--- @return The cross-track error.
---]]
-function funcs:crossTrackError(velocity, gain, p_x, p_y, vh_x, vh_y)
-    local cross_track_error = funcs:haversineDistance(vh_x, vh_y, p_x, p_y)
-    if cross_track_error == nil then
-      return 0
-    end
-    if velocity == nil or velocity < 0.1 then
-      return 0
-    end
-    -- Calculate the steering correction based on the cross-track error
-    local steering_correction = gain * cross_track_error / velocity
-    return steering_correction
-end
-
---[[
-Determines if a vehicle is to the left or right of a line segment.
--- @param start_x The lon coordinate of the start point.
--- @param start_y The lat coordinate of the start point.
--- @param end_x The lon coordinate of the end point.
--- @param end_y The lat coordinate of the end point.
--- @param vehicle_x The lon coordinate of the vehicle.
--- @param vehicle_y The lat coordinate of the vehicle.
--- @return 1 if left, -1 if right
---]]
-function funcs:lineSideSignal(start_x, start_y, end_x, end_y, vehicle_x, vehicle_y)
-    -- Vectors
-    local dx1 = end_x - start_x
-    local dy1 = end_y - start_y
-    local dx2 = vehicle_x - start_x
-    local dy2 = vehicle_y - start_y
-    -- Cross product
-    local cross = dx1 * dy2 - dy1 * dx2
-    if cross > 0 then
-        return 1   -- left
-    elseif cross < 0 then
-        return -1  -- right
-    else
-        return 0   -- collinear
-    end
 end
 
 return funcs
--- Rover/scripts/rover-controlallocation.lua
+++ Rover/scripts/rover-controlallocation.lua
@@ -10,7 +10,6 @@
 --@return number|nil
 -- External modules
 package.path = package.path .. ';./scripts/modules/?.lua'
-local PID = require("pid")
 local funcs = require("functions")
 -------------------------------------------------------------------------------
 ------------------------- GLOBAL SCOPE DEFINITIONS ----------------------------
@@ -21,16 +20,12 @@ local CONTROL_OUTPUT_YAW = 4
 local MAX_CHANNEL_OUTPUT = 1950
 local MIN_CHANNEL_OUTPUT = 1050
 local PWM_RANGE = 450
-local current_mission_index = -1 -- The index we have just reached in the mission
-local current_mission_index_controls_reset = -1 -- The index we have just reached in the mission to reset controls
 -- Vehicle type control
 local VEHICLE_TYPE = param:get('SCR_USER5')
 -- TRIM values
 local PWM_TRIM_VALUE = tonumber(param:get('SERVO1_TRIM')) or 0
 local RC1_TRIM_VALUE = param:get('RC1_TRIM')
 local RC3_TRIM_VALUE = param:get('RC3_TRIM')
--- DEAD ZONE thresh
-local DEAD_ZONE_THRESH = 40
 -- Signal smoothing logic
 local last_manual_throttle = 0
 local throttle_accel_rate_thresh = 0.5
@@ -38,17 +33,6 @@ local throttle_accel_rate = 0.5
 local last_manual_steering = 0
 local steering_accel_rate_thresh = 0.6
 local steering_accel_rate = 0.6
--- Mission control logic - waypoints XY coordinates to calculate bearing error and setpoints
-local current_wp_lat, current_wp_lon = 0, 0
-local last_wp_lat, last_wp_lon = 0, 0
--- Distance to consider a waypoint reached [m]
-local thresh_dist_wp_reached = 2
--- Dead zone for steering when very close to waypoint [m]
-local thresh_dist_steering_deadzone = 0.5
--- PIDs
--- Params: p_gain, i_gain, d_gain, i_max, i_min, pid_max, pid_min
-local ss_pid = PID:new(0.050, 0, 0.010, 90, -90, 0.99, -0.99)  -- for simple setpoint control
-local lc_pid = PID:new(0.001, 0.03, 0.0, 90, -90, 0.99, -0.99) -- for line setpoint control
 -- Severity for logging in GCS
 MAV_SEVERITY = { EMERGENCY = 0, ALERT = 1, CRITICAL = 2, ERROR = 3, WARNING = 4, NOTICE = 5, INFO = 6, DEBUG = 7 }
 -- Rover driving modes
@@ -117,113 +101,6 @@ local function applyPWMManualMode()
   applyControlAllocation(throttle, steering)
 end
 
---[[
-Calculates a line and a bearing from the vehicle to the line projection
---]]
-local function getLineBearingFromWaypoints()
-  local mission_state = mission:state()
-  -- Check if mission is over
-  if mission_state == MISSION_STATE.FINISHED then
-    local steering = 0
-    local throttle = 0
-    vehicle:set_mode(0)
-    current_mission_index = -1
-
-    return steering, throttle
-  end
-
-  -- First time when we start the mission, use current vehicle position as waypoint
-  if current_mission_index == -1 then
-    local vh_location = ahrs:get_position()
-    if vh_location then
-      last_wp_lat = vh_location:lat() / 1e7
-      last_wp_lon = vh_location:lng() / 1e7
-      
-      current_mission_index = mission:get_current_nav_index()
-      local current_waypoint = mission:get_item(current_mission_index)
-      current_wp_lat = current_waypoint:x() / 1e7
-      current_wp_lon = current_waypoint:y() / 1e7
-    end
-  end
-
-  -- If we switched waypoints, refresh the last and current waypoints to form the line
-  local mission_index = mission:get_current_nav_index()
-  if mission_index ~= current_mission_index then
-    -- Update the last and current waypoints
-    last_wp_lat = current_wp_lat
-    last_wp_lon = current_wp_lon
-    current_mission_index = mission_index
-    local current_waypoint = mission:get_item(mission_index)
-    if current_waypoint then
-      current_wp_lat = current_waypoint:x() / 1e7
-      current_wp_lon = current_waypoint:y() / 1e7
-    end
-  end
-
-  -- Get vehicle location info
-  local vh_location = ahrs:get_position()
-  local vh_lon = vh_location:lng() / 1e7
-  local vh_lat = vh_location:lat() / 1e7
-  local vh_yaw = funcs:mapTo360(funcs:toDegrees(ahrs:get_yaw()))
-
-  -- Vehicle velocity info
-  local vh_velocity = ahrs:groundspeed_vector()
-  local vh_velocity_norm = math.sqrt(vh_velocity:x() ^ 2 + vh_velocity:y() ^ 2)
-
-  -- In case of any nil value from the internal state, do not proceed yet
-  if vh_lon == nil or current_wp_lat == nil or last_wp_lat == nil then
-    return 0
-  end
-
-  local line_point_lon, line_point_lat = funcs:lineProjectionPoint(vh_lon, vh_lat, last_wp_lon, last_wp_lat,
-    current_wp_lon, current_wp_lat)
-  -- The bearing angle between the last and current waypoints
-  local wp_line_bearing = funcs:calculateBearingBetweenPoints(last_wp_lat, last_wp_lon, current_wp_lat,
-    current_wp_lon)
-  if wp_line_bearing == 90.0 then
-    return 0 -- Case of very close waypoints
-  end
-  local heading_error = funcs:mapErrorToRange(wp_line_bearing - vh_yaw)
-  -- Calculate the cross track error from the vehicle to the line between waypoints
-  local cross_track_error_gain = param:get('SCR_USER6')
-  local cross_track_error = funcs:crossTrackError(vh_velocity_norm, cross_track_error_gain, line_point_lat, line_point_lon, vh_lat, vh_lon)
-  local cross_track_error_sign = funcs:lineSideSignal(last_wp_lon, last_wp_lat, current_wp_lon, current_wp_lat, vh_lon, vh_lat)
-  -- Return the steering error as the sum of both errors
-  local steering_error = funcs:mapErrorToRange(heading_error + cross_track_error_sign * cross_track_error)
-
-  return steering_error
-end
--------------------------------------------------------------------------------
-
--------------------------------------------------------------------------------
---------------------------- MISSION CONTROL SECTION ---------------------------
--------------------------------------------------------------------------------
---[[
-Control the outputs using only the bearing to the next waypoint
---]]
-local function simpleSetpointControl()
-  -- We have the yaw returned in degrees, 0 degrees to the North, 90 to the East
-  -- The bearing to waypoint has its origin on the vehicle, also following North-East frame
-  local wp_bearing = vehicle:get_wp_bearing_deg()
-  local vh_yaw = funcs:mapTo360(ahrs:get_yaw() * 180.0 / 3.1415)
-  local steering_error = funcs:mapErrorToRange(wp_bearing - vh_yaw)
-
-  -- Compute the steering command signal with the PID simple setpoint controller
-  local steering = ss_pid:compute(steering_error, 0.2)
-  return steering
-end
-
---[[
-Controls the actions by considering the mission previous and current waypoint,
-and the line between them
---]]
-local function followLineControl()
-  local steering_error = getLineBearingFromWaypoints()
-  local steering = lc_pid:compute(steering_error, 0.2)
-  return steering
-end
--------------------------------------------------------------------------------
-
 -------------------------------------------------------------------------------
 -------------------------------- MAIN LOOP ------------------------------------
 -------------------------------------------------------------------------------
@@ -233,11 +110,6 @@ local function update()
     gcs:send_text(MAV_SEVERITY.WARNING, string.format("Not ROVER, exiting LUA script."))
     return
   end
-
-  -- Getting SCR_USER params to PID values
-  local p, i, d = param:get('SCR_USER2') / 1000, param:get('SCR_USER3') / 1000, param:get('SCR_USER4') / 1000
-  -- ss_pid:setGains(p, i, d)
-  lc_pid:setGains(p, i, d)
 
   -- Run not armed routine to guarantee trim values
   if not arming:is_armed() then
@@ -249,22 +121,12 @@ local function update()
     --[[
     Controlling in MANUAL MODE
     --]]
-    -- Reseting control variables
-    current_mission_index = -1
-    current_wp_lat, current_wp_lon = 0, 0
-    last_wp_lat, last_wp_lon = 0, 0
-    -- Resetting PIDs
-    ss_pid:resetInternalState()
-    lc_pid:resetInternalState()
     applyPWMManualMode()
     return update, 200
   elseif vehicle:get_mode() == DRIVING_MODES.HOLD then
     --[[
     Controlling in HOLD MODE
     --]]
-    -- Resetting PIDs
-    ss_pid:resetInternalState()
-    lc_pid:resetInternalState()
     -- Make the vehicle stop
     applyControlAllocation(0, 0)
     return update, 200
@@ -272,18 +134,6 @@ local function update()
     --[[
     Controlling in AUTO MODE
     --]]
-
-    -- Check if the vehicle waypoint has changed, and if so,
-    -- reset the PIDs internal state to avoid spikes and send a 0 signal to the motors
-    local mission_index = mission:get_current_nav_index()
-    if mission_index ~= current_mission_index_controls_reset and mission_index > 0 then
-      ss_pid:resetInternalState()
-      lc_pid:resetInternalState()
-      applyControlAllocation(0, 0)
-      current_mission_index_controls_reset = mission_index
-      return update, 200
-    end
-
     -- Controls end of mission
     local mission_state = mission:state()
     if mission_state == MISSION_STATE.FINISHED then
@@ -292,39 +142,11 @@ local function update()
       return update, 200
     end
 
-    -- Acquiring throttle from internal control output
+    -- Acquiring throttle and steering from internal control output
     local throttle = tonumber(vehicle:get_control_output(THROTTLE_CONTROL_OUTPUT_CHANNEL))
     throttle = funcs:mapMaxMin(throttle, 0.1, 1.0)
     local steering = tonumber(vehicle:get_control_output(CONTROL_OUTPUT_YAW)) or 0
-
-    -- Getting steering from two methods:
-    -- We should pursue both the final waypoint and the current location projected in the line
-    -- That guarantees a fast response, but also a smooth transition between waypoints
-    -- The weighted sum of these values should be the final steering command
-    -- local ss_rate = param:get('SCR_USER1')
-    -- local ss_steering = simpleSetpointControl()
-    -- local lc_rate = 1.0 - ss_rate
-    -- local lc_steering = followLineControl()
-
-    -- -- Fetch the current and target position of the vehicle
-    -- local vehicle_position = ahrs:get_position()
-    -- local target_position = vehicle:get_target_location()
-    -- -- Verify which error to consider for steering
-    -- local steering_error = 0 -- Should be 0 if close enough to the waypoint indeed
-    -- if vehicle_position and target_position then
-    --   -- Calculate the distance to the target (in meters) and choose which controller to use
-    --   local dist = vehicle_position:get_distance(target_position)
-    --   gcs:send_text(MAV_SEVERITY.WARNING, string.format("Dist to WP: %.1f m", dist))
-    --   if dist > thresh_dist_wp_reached then
-    --     steering_error = ss_rate * ss_steering + lc_rate * lc_steering
-    --   end
-    -- else
-    -- steering_error = ss_rate * ss_steering + lc_rate * lc_steering
-    -- end
-    -- local steering_error = ss_rate * ss_steering + lc_rate * lc_steering
-
-    -- -- Apply the control allocation finally
-    -- applyControlAllocation(throttle, steering_error)
+    -- Apply the control allocation finally
     applyControlAllocation(throttle, steering)
 
     return update, 200
